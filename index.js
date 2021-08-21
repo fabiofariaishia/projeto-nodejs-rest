b5992697888c472d577eeb8f1de1bf64988e8876
Fabio Faria <fhabyomontilla@gmail.com>
2021-08-21 15:49:30 -0300

    Primeiro commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express')
+
+const app = express();
+const porta = 3000;
+
+app.listen(porta, () => console.log('servidor rodando na porta ' + porta));
+
+app.get('/atendimentos', (req, res) => res.send('Você está na rota de atendimentos'));
